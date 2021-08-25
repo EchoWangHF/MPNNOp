083e23cb9fc0a4ce1c8910c906005539de154059
wangjun <1427014893@qq.com>
2021-08-25 09:22:49 +0800

    fix(tensor): fix tensor some bug

--- kernels/include/core/tensor.h
+++ kernels/include/core/tensor.h
@@ -9,39 +9,42 @@
 
 #include <assert.h>
 #include <stdint.h>
+#include <string.h>
 #include <vector>
 
 #include "core/core.h"
 
 namespace MNNL {
-class tensorDesc {
+class Tensor {
  public:
-  tensorDesc() : _dtype(DTYPE_FLOAT32), _layout(Layout_ARRAY), _dims_size(0){};
+  Tensor() : _dtype(DTYPE_FLOAT32), _layout(Layout_ARRAY), _dims_size(0){};
 
-  //tensorDesc(const DataType &dtype, const Layout &layout,
-  //           const uint32_t &dims_size, const std::vector<uint32_t> &dims)
-  //    : _dtype(dtype), _layout(layout), _dims_size(dim_size), _dims(dims){};
+  Tensor(const DataType &dtype, const Layout &layout, const uint32_t &dims_size,
+         const std::vector<uint32_t> &dims)
+      : _dtype(dtype), _layout(layout), _dims_size(dims_size), _dims(dims){};
 
-  //tensorDesc(const DataType &dtype, const Layout &layout,
-  //           const std::vector<uint32_t> &dims)
-  //    : _dtype(dtype), _layout(layout), _dims_size(dims.size()), _dims(dims){};
+  Tensor(const DataType &dtype, const Layout &layout,
+         const std::vector<uint32_t> &dims)
+      : _dtype(dtype), _layout(layout), _dims_size(dims.size()), _dims(dims){};
 
-  //tensorDesc(const DataType &dtype, const Layout &layout,
-  //           const uint32_t &dims_size, const uint32_t *dims[]);
+  Tensor(const DataType &dtype, const Layout &layout, const uint32_t &dims_size,
+         const uint32_t *dims[]);
 
-  //~tensorDesc(){};
+  virtual ~Tensor(){
 
-  //DataType getDataType() const { return dtype; }
-  //void setDataType(const DataType &dtype) { _dtype = dtype; }
+  };
 
-  //Layout getLayout() const { return _layout; }
-  //void setLayout(const Layout &layout) { _layout = layout; }
+  DataType getDataType() const { return _dtype; }
+  void setDataType(const DataType &dtype) { _dtype = dtype; }
 
-  //uint32_t getDimSize() const { return _dims_size; }
-  //void setDimSize(const &uint32_t dim_size) { _dims_size = dim_size; }
+  Layout getLayout() const { return _layout; }
+  void setLayout(const Layout &layout) { _layout = layout; }
 
-  //std::vector<uint32_t> getDims const { return _dims; }
-  //void setDims(const std::vector<uint32_t> &dims) { _dims = dims; }
+  uint32_t getDimSize() const { return _dims_size; }
+  void setDimSize(const uint32_t &dim_size) { _dims_size = dim_size; }
+
+  std::vector<uint32_t> getDims() const { return _dims; }
+  void setDims(const std::vector<uint32_t> &dims) { _dims = dims; }
 
  private:
   DataType _dtype;
--- kernels/src/core/tensor.cpp
+++ kernels/src/core/tensor.cpp
@@ -7,12 +7,13 @@
 #include "core/tensor.h"
 
 namespace MNNL {
-//tensorDesc::tensorDesc(const DataType &dtype, const Layout &layout,
-//                       const uint32_t &dims_size, const uint32_t *dims[])
-//    : _dtype(dtype), _layout(layout), _dims_size(dims_size) {
-//  assert(dims != nullptr);
-//  assert(dim_size == sizeof(dims) / sizeof(dims[0]));
-//  std::vector<uint32_t> dims_temp(dims, dims + dims_size);
-//  _dims = dims_temp;
-//}
+Tensor::Tensor(const DataType &dtype, const Layout &layout,
+               const uint32_t &dims_size, const uint32_t *dims[])
+    : _dtype(dtype),
+      _layout(layout),
+      _dims_size(dims_size),
+      _dims(std::vector<uint32_t>(dims_size, 0)) {
+  assert(dims != nullptr);
+  memcpy(_dims.data(), dims, dims_size * sizeof(uint32_t));
+}
 };  // namespace MNNL
